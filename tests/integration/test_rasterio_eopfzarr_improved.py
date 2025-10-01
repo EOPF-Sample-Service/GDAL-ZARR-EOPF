33781716ae39650a77f1273d818e36881cbabbd9
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2025-10-01 17:14:14 +0200

    changed the url

--- tests/integration/test_rasterio_eopfzarr_improved.py
+++ tests/integration/test_rasterio_eopfzarr_improved.py
@@ -29,7 +29,7 @@ pytestmark = pytest.mark.require_driver("EOPFZARR")
 
 # Test URLs
 REMOTE_SAMPLE_ZARR = "https://objects.eodc.eu/e05ab01a9d56408d82ac32d69a5aae2a:202506-s02msil1c/25/products/cpm_v256/S2C_MSIL1C_20250625T095051_N0511_R079_T33TWE_20250625T132854.zarr"
-REMOTE_WITH_SUBDATASETS_ZARR = "https://objects.eodc.eu/e05ab01a9d56408d82ac32d69a5aae2a:202507-s02msil2a/21/products/cpm_v256/S2B_MSIL2A_20250721T073619_N0511_R092_T36HUG_20250721T095416.zarr/conditions/mask/detector_footprint/r10m/b04"
+REMOTE_WITH_SUBDATASETS_ZARR = "https://objects.eodc.eu/e05ab01a9d56408d82ac32d69a5aae2a:202506-s02msil1c/25/products/cpm_v256/S2C_MSIL1C_20250625T095051_N0511_R079_T33TWE_20250625T132854.zarr/measurements/reflectance/r60m/b01"
 
 
 # Environment Detection and Configuration
